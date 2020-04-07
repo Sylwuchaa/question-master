322dab4203422c55e64d2039e33a8a0903f5798e
Sylwester <sylwester.srokosz@gmail.com>
2020-04-07 10:13:53 +0200

    refactor method to showing quiz component

--- src/components/PreparingQuiz/PreparingQuiz.tsx
+++ src/components/PreparingQuiz/PreparingQuiz.tsx
@@ -5,6 +5,7 @@ import { AppContainer, ButtonContainer } from '../../styled/components/GlobalCom
 import { Quiz } from '../Quiz'
 import { useGlobalReducers } from '../../hooks/useQuizReducer'
 import { GlobalContext } from '../../App'
+import { Link } from 'react-router-dom'
 
 const Input = styled.input`
   text-align: center;
@@ -38,79 +39,69 @@ export const PreparingQuiz: React.FC = () => {
   const globalContext = useContext(GlobalContext)
 
   // const { quizState, initialState, quizDispatch, inputsDispatch } = useGlobalReducers()
-  
+
   const handleOnChangeInputs = (evt: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     globalContext.inputsDispatch({ field: evt.target.name, value: evt.target.value })
   }
-  const [showQuiz, setShowQuizStatus] = useState<boolean>(false)
 
   return (
-    <>
-      {!showQuiz && (
-        <AppContainer>
-        <h1 className="title">Prepare Your Quiz !</h1>
-        <Label htmlFor="questionNumber">Select number of question/s:</Label>
-        <Input
-          name="numberOfQuestion"
-          className="input is-info"
-          id="questionNumber"
-          type="number"
-          min="1"
-          max="50"
-          placeholder="Between 1 to 50"
-          value={globalContext.initialState.numberOfQuestion}
-          onChange={handleOnChangeInputs}
-          required={true}
+    <AppContainer>
+      <h1 className="title">Prepare Your Quiz !</h1>
+      <Label htmlFor="questionNumber">Select number of question/s:</Label>
+      <Input
+        name="numberOfQuestion"
+        className="input is-info"
+        id="questionNumber"
+        type="number"
+        min="1"
+        max="50"
+        placeholder="Between 1 to 50"
+        value={globalContext.initialState.numberOfQuestion}
+        onChange={handleOnChangeInputs}
+        required={true}
+      />
+      <InputContainer>
+        <SelectCategory
+          handleOnChangeCategory={handleOnChangeInputs}
+          categoryValue={globalContext.initialState.selectedCategory}
         />
-        <InputContainer>
-          <SelectCategory
-            handleOnChangeCategory={handleOnChangeInputs}
-            categoryValue={globalContext.initialState.selectedCategory}
-          />
-        </InputContainer>
-        <InputContainer>
-          <Label htmlFor="difficulty">Select Difficulty:</Label>
-          <SelectInputContainer className="select is-info">
-            <SelectInput
-              value={globalContext.initialState.difficulty}
-              id="difficulty"
-              onChange={handleOnChangeInputs}
-              name="difficulty"
-            >
-              <option>Any Difficulty</option>
-              <option value="easy">Easy</option>
-              <option value="medium">Medium</option>
-              <option value="hard">Hard</option>
-            </SelectInput>
-          </SelectInputContainer>
-        </InputContainer>
-        <InputContainer>
-          <Label htmlFor="type">Select Type:</Label>
-          <SelectInputContainer className="select is-info">
-            <SelectInput
-              id="type"
-              onChange={handleOnChangeInputs}
-              value={globalContext.initialState.typeOfQuiz}
-              name="typeOfQuiz"
-            >
-              <option>Any Type</option>
-              <option value="multiple">Multiple Choice</option>
-              <option value="boolean">True / False</option>
-            </SelectInput>
-          </SelectInputContainer>
-        </InputContainer>
-        <ButtonContainer>
-            <button
-              onClick={() => setShowQuizStatus(true)}
-              className="button is-primary is-large is-rounded">
-              Let's Start !
-            </button>
-        </ButtonContainer>
-      </AppContainer>
-      )}
-      {showQuiz && (
-        <Quiz />
-      )}
-    </>
+      </InputContainer>
+      <InputContainer>
+        <Label htmlFor="difficulty">Select Difficulty:</Label>
+        <SelectInputContainer className="select is-info">
+          <SelectInput
+            value={globalContext.initialState.difficulty}
+            id="difficulty"
+            onChange={handleOnChangeInputs}
+            name="difficulty"
+          >
+            <option>Any Difficulty</option>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </SelectInput>
+        </SelectInputContainer>
+      </InputContainer>
+      <InputContainer>
+        <Label htmlFor="type">Select Type:</Label>
+        <SelectInputContainer className="select is-info">
+          <SelectInput
+            id="type"
+            onChange={handleOnChangeInputs}
+            value={globalContext.initialState.typeOfQuiz}
+            name="typeOfQuiz"
+          >
+            <option>Any Type</option>
+            <option value="multiple">Multiple Choice</option>
+            <option value="boolean">True / False</option>
+          </SelectInput>
+        </SelectInputContainer>
+      </InputContainer>
+      <ButtonContainer>
+        <Link to="/quiz" className="button is-primary is-large is-rounded">
+          Let's Start !
+        </Link>
+      </ButtonContainer>
+    </AppContainer>
   )
 }
